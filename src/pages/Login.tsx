e1e6a1a0dcdcaeadc34be7c30b2bdeaea0df894c
perfumeplaylist <perfumeplaylist@gmail.com>
2025-02-15 15:10:59 +0900

    fix: login 컴포넌트 구조 개선 및 Flex 컴포넌트 사용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,48 +1,57 @@
 import loadingGif from '@/assets/login/channel_login.gif';
+import { YoutubeIcon } from '@/assets/svg/logo/YoutubeIcon';
 import LocationMove from '@/domains/Login/components/LocationMove';
 import useGoogleAuth from '@/domains/Login/hooks/useGoogleAuth';
-import { Spacing, Text } from '@/shared/ui';
+import { Flex, Spacing, Text } from '@/shared/ui';
 
 export default function Login() {
   const { data } = useGoogleAuth();
 
   return (
-    <main className="flex flex-col px-5 pt-5 pb-10 bg-line min-h-[812px]">
-      <section className="text-center">
-        <Text
-          as="title"
-          title="정확한 분석을 위해"
-          className="text-[24px] font-extrabold"
+    <main className="bg-line min-h-[812px]">
+      <Flex direction="column" className="px-5 pt-5 pb-10">
+        <section className="text-center">
+          <Text
+            as="title"
+            title="정확한 분석을 위해"
+            className="text-[24px] font-extrabold"
+          />
+          <Text
+            as="title"
+            title="SNS 계정 연동이 필요해요."
+            className="text-[24px] font-extrabold"
+          />
+        </section>
+        <Spacing size="lsmall" />
+
+        <img
+          src={loadingGif}
+          alt="로그인 gif 애니메이션"
+          className="rounded-xl"
         />
+
+        <Spacing className="h-[76px]" />
+
+        <LocationMove location={data.googleAuthUrl}>
+          <Flex
+            align="center"
+            className="bg-red1 px-[26px] py-3 rounded-[26px]"
+          >
+            <YoutubeIcon />
+            <button className="text-white text-center w-11/12">
+              Youtube 채널 가지고 오기
+            </button>
+          </Flex>
+        </LocationMove>
+
+        <Spacing className="h-[10px]" />
+
         <Text
-          as="title"
-          title="SNS 계정 연동이 필요해요."
-          className="text-[24px] font-extrabold"
+          as="description"
+          title="시작함으로써 이용약관 및 개인정보 수집 및 이용에 동의하게 됩니다."
+          className="text-center text-gray5 px-[26px] text-[11px]"
         />
-      </section>
-      <Spacing size="lsmall" />
-
-      <img src={loadingGif} alt="로그인 gif 애니메이션" />
-
-      <Spacing className="h-[76px]" />
-
-      <LocationMove
-        location={data.googleAuthUrl}
-        className="flex items-center bg-red1 px-[26px] py-3 rounded-[26px]"
-      >
-        {/* <YoutubeIcon /> */}
-        <button className="text-white text-center w-full">
-          Youtube 채널 가지고 오기
-        </button>
-      </LocationMove>
-
-      <Spacing className="h-[10px]" />
-
-      <Text
-        as="description"
-        title="시작함으로써 이용약관 및 개인정보 수집 및 이용에 동의하게 됩니다."
-        className="text-center text-gray5 px-[26px] text-[11px]"
-      />
+      </Flex>
     </main>
   );
 }
